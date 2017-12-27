182184812cb4b03e97322f30e705417910d6703d
julia- <julia.ryanx@gmail.com>
2017-12-27 17:55:41 +1100

    implement removeFish method to remove Fish from state via button in Inventory component

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -58,6 +58,7 @@ class Inventory extends React.Component {
           placeholder="Fish Image"
           onChange={e => this.handleChange(e, key)}
         />
+        <button onClick={() => this.props.removeFish(key)}>Remove</button>
       </div>
     )
   }
