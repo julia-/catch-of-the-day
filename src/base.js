662d963f89006a09d3e14a034863d29d1a301742
julia- <julia.ryanx@gmail.com>
2017-12-27 15:51:41 +1100

    persist the fish state using firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyAsWhxESUueaWLEgmZqyoByo9tMQ2nVNVA',
+  authDomain: 'catch-of-the-day-julia.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-julia.firebaseio.com'
+})
+
+export default base
