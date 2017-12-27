d6a2d2f8ac3cbd0c7fba52b03b765f1ecbbdf2c1
julia- <julia.ryanx@gmail.com>
2017-12-27 22:17:38 +1100

    use CSSTransitionGroup to animate elements in Order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { formatPrice } from '../helpers'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 class Order extends React.Component {
   constructor() {
@@ -10,23 +11,33 @@ class Order extends React.Component {
   renderOrder(key) {
     const fish = this.props.fishes[key]
     const count = this.props.order[key]
-    const removeButton =
-      <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+    const removeButton = <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
 
     if (!fish || fish.status === 'unavailable') {
       return (
         <li key={key}>
-          Sorry, {fish ? fish.name : 'fish'} is no longer available{' '}
+          Sorry, {fish ? fish.name : 'fish'} is no longer available
           {removeButton}
         </li>
       )
     }
-    return <li key={key}>
+    return (
+      <li key={key}>
         <span>
-          {count}lbs {fish.name} {removeButton}
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+          >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
         </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
       </li>
+    )
   }
   render() {
     const orderIds = Object.keys(this.props.order)
@@ -42,12 +53,18 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your order</h2>
-        <ul className="order">
+        <CSSTransitionGroup
+          component="ul"
+          className="order"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>{formatPrice(total)}</strong>
           </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     )
   }
