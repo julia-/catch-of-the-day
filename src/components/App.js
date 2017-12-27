a020789a6a5413515dd440556ad5c5f46ca8ec85
julia- <julia.ryanx@gmail.com>
2017-12-27 12:34:17 +1100

    create addToOrder method and pass to fish component via props

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     super()
     this.addFish = this.addFish.bind(this)
     this.loadSamples = this.loadSamples.bind(this)
+    this.addToOrder = this.addToOrder.bind(this)
     // initial state
     this.state = {
       fishes: {},
@@ -30,13 +31,28 @@ class App extends React.Component {
     })
   }
 
+  addToOrder(key) {
+    // get copy of the state
+    const order = { ...this.state.order }
+    // update or add the number of fish ordered
+    order[key] = order[key] + 1 || 1
+    // update state
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
         <Order />
