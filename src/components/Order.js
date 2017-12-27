565d3657f7c4c598695527b2f12ca0c5ca840fcf
julia- <julia.ryanx@gmail.com>
2017-12-27 14:48:49 +1100

    display total price in Order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { formatPrice } from '../helpers'
 
 class Order extends React.Component {
   render() {
-    return <p>Order</p>
+    const orderIds = Object.keys(this.props.order)
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key]
+      const count = this.props.order[key]
+      const isAvailable = fish && fish.status === 'available'
+      if (isAvailable) {
+        return prevTotal + (count * fish.price || 0)
+      }
+      return prevTotal
+    }, 0)
+    return (
+      <div className="order-wrap">
+        <h2>Your order</h2>
+        <ul className="order">
+          <li className="total">
+            <strong>{formatPrice(total)}</strong>
+          </li>
+        </ul>
+      </div>
+    )
   }
 }
 
